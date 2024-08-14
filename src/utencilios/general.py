4831d8d00b552311b98d60e085515fd96d393256
Anthony Yoskovich <anthony.yoskovich@gmail.com>
2024-08-14 08:22:40 -0500

    Adds print_params function

--- src/utencilios/general.py
+++ src/utencilios/general.py
@@ -166,8 +166,7 @@ class DataFrameDiffer:
     - Matching columns
     - Filtering to differing rows
 
-    Examples
-
+    Example:
         .. ipython:: python
 
             df1 = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
@@ -323,3 +322,39 @@ def pathsafenow() -> str:
     now = datetime.now()
     formatted_datetime = now.strftime("%Y-%m-%d_%H-%M-%S")
     return formatted_datetime
+
+
+def print_params(obj, *args) -> None:
+    """Prints out the value of each arg from obj
+
+
+    Example:
+        .. ipython:: python
+
+            class Person:
+                def __init__(self, name, age):
+                    self.name = name
+                    self.age = age
+                def say_hello(self, formal=False):
+                    if formal:
+                        return f'Welcome, {self.name}.'
+                    else:
+                        return f'Hey {self.name}!'
+            person = Person('spongebob', 100)
+            ut.print_params(person, 'name', 'age', 'say_hello()', {'say_hello': {'formal': True}})
+    """
+    print(f"Summary for {obj}")
+    for arg in args:
+        if isinstance(arg, dict):
+            keys = list(arg.keys())
+            if len(keys) > 1:
+                raise ValueError("Method dicts are expected to have a single key!")
+            method_name = keys[0].replace("()", "")
+            method_kwargs = arg[method_name]
+            val = getattr(obj, method_name)(**method_kwargs)
+        elif arg.lower().endswith("()"):
+            method_name = arg.replace("()", "")
+            val = getattr(obj, method_name)()
+        else:
+            val = getattr(obj, arg)
+        print(f"{arg}: {val}")
